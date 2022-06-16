5b9fb0a5053fcc3f8536d4068e58c9b01da51e65
LRamson <lucasramson@gmail.com>
2022-06-16 16:21:01 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Header } from './components/Header.jsx';
+import { Post } from './Post.jsx';
+
+import styles from './App.module.css';
+import './global.css';
+import { Sidebar } from './components/Sidebar.jsx';
+
+export function App() {
+  return (
+    <div>
+      <Header/>
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Lucas Ramson"
+            content="Teste 123 post bem secreto"
+          />
+          <Post 
+            author="Lcas Raumson"
+            content=" 123 post Testebem secreto"
+          />
+          <Post />
+          <Post />
+        </main>
+      </div>
+    </div>
+  )
+}
