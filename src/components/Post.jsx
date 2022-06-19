40ed892847357ac272000897797cd26ca20019d4
LRamson <lucasramson@gmail.com>
2022-06-19 15:29:26 -0300

    Adicionada validação de comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -16,6 +16,8 @@ export function Post({ author, publishedAt, content}) {
 
     const [newCommentText, setNewCommentText] = useState('')
 
+    console.log(newCommentText);
+
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
     })
@@ -33,7 +35,12 @@ export function Post({ author, publishedAt, content}) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Este campo é obrigatório!');
     }
 
     function deleteComment(commentToDelete) {
@@ -44,6 +51,8 @@ export function Post({ author, publishedAt, content}) {
         
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
 
     return (
         <article className={styles.post}>
@@ -78,10 +87,16 @@ export function Post({ author, publishedAt, content}) {
                  placeholder='Deixe um comentário'
                  value={newCommentText}
                  onChange={handleNewCommentChange}
+                 onInvalid={handleNewCommentInvalid}
+                 required
                 />
 
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button 
+                     type='submit'
+                     disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
